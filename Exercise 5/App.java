82243fa80c146b296b7782b764939eba5702b5c6
aldrexsuarez <122065743+aldrexsuarez@users.noreply.github.com>
2023-02-10 10:06:28 +0800

    Create App.java

---
+++ Exercise 5/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Person Me = new Person();
+
+        // The name property cannot be accessed outside of the Person class.
+        // Me.name;
+
+        Me.setName("Aldrex");
+
+        String myName = Me.getName();
+
+        System.out.println("My name is " + myName);
+
+        Me.setCCnumber(12345);
+
+        int myCCnumber = Me.getCCnumber();
+        
+        System.out.println("My cc number is " + myCCnumber);
+
+        //==================================================
+
+        Son me = new Son();
+
+        System.out.println("My family name is: " + me.surname);
+
+        me.showLove();
+    }
+}
